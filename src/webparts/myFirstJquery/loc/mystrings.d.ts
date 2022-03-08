ac5db41c19be5f59387eeb638ef76423886aa192
Rishabh Sharma <rsharma4125@gmail.com>
2022-03-08 09:43:10 +0530

    init

---
+++ src/webparts/myFirstJquery/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMyFirstJqueryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MyFirstJqueryWebPartStrings' {
+  const strings: IMyFirstJqueryWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/myFirstJqueryWebpart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMyFirstJqueryWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MyFirstJqueryWebpartWebPartStrings' {
+  const strings: IMyFirstJqueryWebpartWebPartStrings;
+  export = strings;
+}
